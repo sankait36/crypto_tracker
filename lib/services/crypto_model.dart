e3f32dde90e045c7cc451e901f32cba6e7807030
sankait36 <sankait36@hotmail.com>
2019-09-01 17:09:56 -0500

    Updated crypto model

--- lib/services/crypto_model.dart
+++ lib/services/crypto_model.dart
@@ -1,15 +1,31 @@
 import 'package:crypto_tracker/services/networking.dart';
 
-const bitcoinAverageAPIBase = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/short';
+const bitcoinAverageAPIBase =
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker/short';
 
 // https://apiv2.bitcoinaverage.com/indices/global/ticker/short?crypto=BTC,ETH,LTC&fiat=USD,EUR,AUD
 
 class CryptoModel {
-  Future<dynamic> getCryptoData(List<String> cryptoList, List<String> currencyList) async {
+  Future<Object> getCryptoData(
+      List<String> cryptoList, List<String> currencyList) async {
     String cryptos = cryptoList.join(',');
     String fiats = currencyList.join(',');
-    Networking networkHelper = Networking('$bitcoinAverageAPIBase?crypto=$cryptos&fiat=$fiats');
+    Networking networkHelper =
+        Networking('$bitcoinAverageAPIBase?crypto=$cryptos&fiat=$fiats');
     var cryptoData = await networkHelper.getData();
+    getPricesForCryptoToken(cryptoList, cryptoData);
     return cryptoData;
+  }
+
+  Object getPricesForCryptoToken(dynamic cryptoList, dynamic cryptoData) {
+    var result = {};
+    for (String item in cryptoList) {
+      result[item] = {};
+    }
+    for (String data in cryptoData.keys) {
+      result[data.substring(0, 3)][data.substring(3)] = cryptoData[data]['ask'];
+    }
+    print(result);
+    return result;
   }
 }
